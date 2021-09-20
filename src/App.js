53609e3162978163d2545e3fab2e0561e665bf6e
nmnsharma007 <nmnsharma20@gmail.com>
2021-09-20 11:46:06 +0530

    counter app complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from "react";
 
 function App() {
+
+  const [counterValue, setcounterValue] = useState(1);
+  const [changeValue,setChangeValue] = useState(1);
+
+  useEffect(() => {
+    fetch("/data.json").then((response)=>response.json()).then((data) => {
+      setcounterValue(data.counter);
+      setChangeValue(data.changeValue);
+    }).catch(err => console.log(err))
+    
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className="App">
+    <input value={changeValue} type="number"onChange={(e) => {setChangeValue(parseInt(e.target.value))}}/>
+      <div className="counter">
+      <button onClick={() => setcounterValue((prevValue) => prevValue + changeValue)}>+</button>
+      <p>{counterValue}</p>
+      <button onClick={() => setcounterValue((prevValue) => prevValue - changeValue)}>-</button>
+      </div>
     </div>
   );
 }
